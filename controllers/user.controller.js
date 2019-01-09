777d50141cb55c6e75a8ce5f91cd44f27f79ed83
Toan Dang <ddtoan991205@gmail.com>
2019-01-10 00:01:25 +0700

    Lesson 9 - Controller (MVC)

---
+++ controllers/user.controller.js
@@ -0,0 +1,41 @@
+var db = require('../db');
+var shortid = require('shortid');
+
+module.exports.index = function(req, res){
+    res.render('users/index', {
+        users: db.get('users').value()
+    });
+}
+
+module.exports.search = (req, res) => {
+    var q = req.query.q;
+    var matchedUsers =  db.get('users').value().filter(function(user){
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    });
+    console.log(matchedUsers);
+    res.render('users/index', {
+        users: matchedUsers
+    });
+    console.log(req.query); 
+}
+
+module.exports.create = (req, res)=>{
+    
+    res.render('users/create');
+}
+
+module.exports.get = (req, res)=>{
+    // var id = parseInt(req.params.id); Bỏ đi vì dùng shortid rồi!
+    var id = req.params.id;
+    var user = db.get('users').find({ id: id}).value();
+
+    res.render('users/view', {
+        user: user
+    });
+}
+
+module.exports.postCreate =  function(req, res){
+    req.body.id = shortid.generate();
+    db.get('users').push(req.body).write();
+    res.redirect('/users');
+}
--- routes/user.route.js
+++ routes/user.route.js
@@ -2,45 +2,17 @@ var express = require('express');
 var shortid = require('shortid');
 
 var db = require('../db');
+var controller = require('../controllers/user.controller');
 
 var router = express.Router();
-router.get('/', function(req, res){
-    res.render('users/index', {
-        users: db.get('users').value()
-    });
-})
+router.get('/', controller.index);
 
-router.get('/search', (req, res) => {
-    var q = req.query.q;
-    var matchedUsers =  db.get('users').value().filter(function(user){
-        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-    });
-    console.log(matchedUsers);
-    res.render('users/index', {
-        users: matchedUsers
-    });
-    console.log(req.query); 
-})
+router.get('/search', controller.search);
 
-router.get('/create', (req, res)=>{
-    
-    res.render('users/create');
-})
+router.get('/create', controller.create);
 
-router.get('/:id', (req, res)=>{
-    // var id = parseInt(req.params.id); Bỏ đi vì dùng shortid rồi!
-    var id = req.params.id;
-    var user = db.get('users').find({ id: id}).value();
+router.get('/:id', controller.get);
 
-    res.render('users/view', {
-        user: user
-    });
-})
-
-router.post('/create', function(req, res){
-    req.body.id = shortid.generate();
-    db.get('users').push(req.body).write();
-    res.redirect('/users');
-})
+router.post('/create', controller.postCreate);
 
 module.exports = router;
