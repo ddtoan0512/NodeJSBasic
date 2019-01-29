8e22a5025a3f5de0e97b94d5a518e7de05d9189e
Toan Dang <ddtoan991205@gmail.com>
2019-01-29 19:16:17 +0700

    Lesson 16 - MD5

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -20,7 +20,6 @@ module.exports.search = (req, res) => {
 }
 
 module.exports.create = (req, res)=>{
-    console.log(req.cookies);
     res.render('users/create');
 }
 
@@ -36,9 +35,7 @@ module.exports.get = (req, res)=>{
 
 module.exports.postCreate =  function(req, res){
     req.body.id = shortid.generate();
-
     
-
     db.get('users').push(req.body).write();
     res.redirect('/users');
 }
